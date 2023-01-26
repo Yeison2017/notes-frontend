16397b429817efcfe7ed28efe3502a7fe335befc
yeison2017 <yha.big@gmail.com>
2023-01-26 11:47:29 -0500

    Se aplican estilos en los en el card

--- src/app/pages/notes-list/notes-list.component.ts
+++ src/app/pages/notes-list/notes-list.component.ts
@@ -3,8 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-notes-list',
   templateUrl: './notes-list.component.html',
-  styleUrls: ['./notes-list.component.scss']
+  styleUrls: ['./notes-list.component.scss'],
 })
 export class NotesListComponent {
-
+  cardTitle: string = 'Notes List';
+  cardBody: string = 'Notes List';
 }
