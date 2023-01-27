68b1aee002c1ff283fce4cc90b49508a1d750a65
yeison2017 <yha.big@gmail.com>
2023-01-27 11:14:55 -0500

    Se crea un servicio para manejara las notas

--- src/app/pages/note-details/note-details.component.ts
+++ src/app/pages/note-details/note-details.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Note } from 'src/app/shared/note.model';
 
 @Component({
   selector: 'app-note-details',
   templateUrl: './note-details.component.html',
-  styleUrls: ['./note-details.component.scss']
+  styleUrls: ['./note-details.component.scss'],
 })
-export class NoteDetailsComponent {
+export class NoteDetailsComponent implements OnInit {
+  note: Note = new Note();
 
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  onSubmit(form: NgForm) {}
 }
