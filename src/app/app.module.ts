95fb141d8305e414b2b352f029739b027449cb8f
yeison2017 <yha.big@gmail.com>
2023-02-07 13:52:15 -0500

    Se realiza animacion en lastarjetas de las notas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -17,7 +18,12 @@ import { NoteDetailsComponent } from './pages/note-details/note-details.componen
     NoteCardComponent,
     NoteDetailsComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/pages/notes-list/notes-list.component.ts
+++ src/app/pages/notes-list/notes-list.component.ts
@@ -1,3 +1,11 @@
+import {
+  animate,
+  query,
+  stagger,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 import { Note } from 'src/app/shared/note.model';
 import { NotesService } from 'src/app/shared/notes.service';
@@ -6,6 +14,86 @@ import { NotesService } from 'src/app/shared/notes.service';
   selector: 'app-notes-list',
   templateUrl: './notes-list.component.html',
   styleUrls: ['./notes-list.component.scss'],
+  animations: [
+    trigger('itemAmin', [
+      transition('void => *', [
+        style({
+          height: 0,
+          opacity: 0,
+          tranform: 'scale(0.85)',
+          'margin-bottom': 0,
+
+          paddingTop: 0,
+          paddingBottom: 0,
+          paddingLeft: 0,
+          paddingRight: 0,
+        }),
+        animate(
+          '50ms',
+          style({
+            height: '*',
+            'margin-botton': '*',
+            paddingTop: '*',
+            paddingBottom: '*',
+            paddingLeft: '*',
+            paddingRight: '*',
+          })
+        ),
+        animate(68),
+      ]),
+
+      transition('* => void', [
+        animate(
+          50,
+          style({
+            transform: 'scale(1.05)',
+          })
+        ),
+        animate(
+          50,
+          style({
+            transform: 'sacele(1)',
+            opacity: 0.75,
+          })
+        ),
+        animate(
+          '120ms ease-out',
+          style({
+            transform: 'scale(0.68)',
+            opacity: 0,
+          })
+        ),
+        animate(
+          '150ms ease-out',
+          style({
+            height: 0,
+            paddingTop: 0,
+            paddingBottom: 0,
+            paddingLeft: 0,
+            paddingRight: 0,
+            'margin-bottom': 0,
+          })
+        ),
+      ]),
+    ]),
+    trigger('listAnim', [
+      transition('* => *', [
+        query(
+          ':enter',
+          [
+            style({
+              opacity: 0,
+              height: 0,
+            }),
+            stagger(100, [animate('0.2s ease')]),
+          ],
+          {
+            optional: true,
+          }
+        ),
+      ]),
+    ]),
+  ],
 })
 export class NotesListComponent implements OnInit {
   notes: Note[] = new Array<Note>();
